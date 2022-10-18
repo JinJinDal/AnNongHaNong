2317aa55e4dda6f8cbe9671fea7f97be46b90f2a
KimJinWoo <100827318+JinJinDal@users.noreply.github.com>
2022-10-18 09:29:06 +0900

    1018
    링크미리보기 추가
    js제거

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-window.addEventListener("DOMContentLoaded",function(){
-    window.onscroll = function(){
-        console.log(window.scrollY);
-        if(window.scrollY > 100){
-            // document.getElementsByClassName("text_box").classList.add("active");
-        }else{
-            // document.getElementsByClassName("text_box").classList.remove("active");
-        }
-    }
-});
+// window.addEventListener("DOMContentLoaded",function(){
+//     window.onscroll = function(){
+//         console.log(window.scrollY);
+//         if(window.scrollY > 100){
+//             document.getElementsByClassName("girl1").classList.add('active');
+//         }else{
+//             // document.getElementsByClassName("text_box").classList.remove("active");
+//         }
+//     }
+// });
+
+// $(document).ready(function(){
+//     $(window).scroll({
+        
+//     });
+// });
+// $(function(){
+//     $('body').scroll(function(){
+//         console.log('hello');
+//     });
+// });
